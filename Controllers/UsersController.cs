074906dccec17019c66235f238cbca68ed773418
Revathi M <revathimuthu2004@gmail.com>
2026-08-22 12:02:20 +0530

    get function

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetUser(
+            [FromQuery] int id,
+            [FromQuery] string name,
+            [FromQuery] string email)
+        {
+            return Ok(new
+            {
+                Id = id,
+                Name = name,
+                Email = email
+            });
+        }
+    }
+}
